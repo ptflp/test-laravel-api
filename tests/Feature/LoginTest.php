bec9263aca2ec2fca2f31f09bcdf0dd0c7e320ad
ptflp <filippov.pa@s-vfu.ru>
2019-08-01 00:26:38 +0900

    upload project

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRequiresEmailAndLogin()
+    {
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                'email' => [ 'The email field is required.' ],
+                'password' => [ 'The password field is required.' ],
+            ]);
+    }
+
+    public function testUserLoginsSuccessfully()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'testlogin@user.com',
+            'password' => bcrypt('toptal123'),
+        ]);
+
+        $payload = ['email' => 'testlogin@user.com', 'password' => 'toptal123'];
+
+        $this->json('POST', 'api/login', $payload)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                    'api_token',
+                ],
+            ]);
+    }
+}
